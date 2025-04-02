02923b8190bdf3e7ebc38d55eb2786b6a6488cc7
Abdallah Abdul <tachincursion@gmail.com>
2025-04-02 15:56:10 +0000

    Initial commit

--- my-web/src/components/footer.jsx
+++ my-web/src/components/footer.jsx
@@ -1,7 +1,7 @@
-export default function Footer() {        
+export default function Footer() {
     return (
         <footer className= "w-full bg-blue-500 text-white p-4 textc-center">
             <p>&copy; {new Date().getFullYear()}My Website</p>   
         </footer>
    );
-   }
+}
