eeb9df3e74cef88625cc310ff343e829510bbf7e
Abdallah Abdul <tachincursion@gmail.com>
2025-04-06 18:37:59 +0300

    Delete my-web directory

--- my-web/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className= "w-full bg-blue-500 text-white p-4 textc-center">
-            <p>&copy; {new Date().getFullYear()}My Website</p>   
-        </footer>
-   );
-}
